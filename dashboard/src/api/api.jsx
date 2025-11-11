bb4403025b50b21e495c0d2998bc0aaa7ecc4048
Aditya Kumar <118003428+Adityakumar92@users.noreply.github.com>
2025-11-11 12:42:51 +0530

    base url add

--- dashboard/src/api/api.jsx
+++ dashboard/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://deepconnection-life.onrender.com',
+  baseURL: import.meta.env.VITE_BASE_URL ||  'http://localhost:8080',
   headers: {
     "Content-Type": "application/json",
   },
